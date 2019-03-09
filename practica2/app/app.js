37a66ec7137b7e1ed587511df32b9f0ea353f57d
root <root@ubuntusopes1.us-east1-b.c.sopes1.internal>
2019-03-09 05:04:00 +0000

    V.2.0.0

--- practica2/app/app.js
+++ practica2/app/app.js
@@ -15,6 +15,8 @@ const path = require('path');
 
 var bodyParser = require('body-parser')
 
+const fs = require('fs');
+
 app.set("view engine","jade"); //definicion del motor de vistas
 
 app.get("/login",function(req,res){
@@ -30,5 +32,52 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/",routers);
+
+
+//var getUsage = function(){
+//    fs.readFile("/proc/" + process.pid + "/stat", function(err, data){
+//        console.log(data.toString());
+//    });
+//}
+
+//getUsage();
+
+var ocioso=0,detenido=0,corriendo=0,zombie=0,suspendido = 0;
+
+const testFolder = '/proc/';
+
+fs.readdir(testFolder, (err, files) => {
+  files.forEach(file => {
+    if(!isNaN(file)){
+       file = testFolder+file;
+       fs.readFile(file+"/stat",function(err,data){
+	    var informacion = data.toString();
+            var elems = data.toString().split(' ');
+	    var estado = elems[2];
+		console.log(estado);
+
+		if(estado == "S"){
+			suspendido +=1;
+		}else if(estado == "I"){
+			ocioso += 1;
+		}else if(estado == "T"){
+			detenido +=1;
+		}else if(estado == "R"){
+			corriendo +=1;
+		}else if(estado == "Z"){
+			zombie +=1;
+		}
+		
+		console.log("REPORTE");
+                console.log("Suspendidos: "+suspendido);
+                console.log("Ociosos: "+ocioso);
+                console.log("Detenido: "+detenido);
+                console.log("Corriendo: "+corriendo);
+
+       });
+    }
+  });
+  
+});
 
 app.listen(8000);
