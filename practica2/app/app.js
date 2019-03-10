2eaa8e51422d3c20cf6045c3f5b934182fc660fd
DanielGarcia097 <2009402600101@ingenieria.usac.edu.gt>
2019-03-09 22:51:28 -0600

    V.2.0.4
    Información en POST

--- practica2/app/app.js
+++ practica2/app/app.js
@@ -15,9 +15,6 @@ const path = require('path');
 
 var bodyParser = require('body-parser')
 
-var userid = require('userid');
-
-const fs = require('fs');
 
 app.set("view engine", "jade"); //definicion del motor de vistas
 
@@ -34,68 +31,5 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/", routers);
-
-
-//var getUsage = function(){
-//    fs.readFile("/proc/" + process.pid + "/stat", function(err, data){
-//        console.log(data.toString());
-//    });
-//}
-
-//getUsage();
-
-var ocioso = 0, detenido = 0, corriendo = 0, zombie = 0, suspendido = 0;
-
-const testFolder = '/proc/';
-
-fs.readdir(testFolder, (err, files) => {
-  files.forEach(file => {
-    if (!isNaN(file)) {
-      var pid = file;
-      file = testFolder + file;
-      fs.readFile(file + "/status", function (err, data) {
-        var elems = data.toString().split('\n');
-        var estado = elems[2];
-        var user = elems[8];
-        var state = estado.split('	')[1].split(' ');
-        var id_user = user.split('	');
-	var name = elems[0].split('	');
-        console.log("uid name is:", userid.username(parseInt(id_user[1])));
-        console.log(id_user[1]);
-	console.log("name process: "+name[1]);
-        console.log("PID: "+pid);
-
-        if (state[0] == "S") {
-          suspendido += 1;
-        } else if (state[0] == "I") {
-          ocioso += 1;
-        } else if (state[0] == "T") {
-          detenido += 1;
-        } else if (state[0] == "R") {
-          corriendo += 1;
-        } else if (state[0] == "Z") {
-          zombie += 1;
-        }
-
-        console.log("REPORTE");
-        console.log("Suspendidos: " + suspendido);
-        console.log("Ociosos: " + ocioso);
-        console.log("Detenido: " + detenido);
-        console.log("Corriendo: " + corriendo);
-
-      });
-
-      fs.readFile(file + "/statm", function (err, data) {
-        var elems = data.toString().split(' ');
-        var memoria = elems[1];
-        console.log(file+" Cantidad memoria: "+memoria+"Porcentaje: "+memoria/10000+"%");
-
-      });
-
-
-    }
-  });
-
-});
 
 app.listen(8000);
--- practica2/app/routes.js
+++ practica2/app/routes.js
@@ -4,20 +4,109 @@ var router = express.Router();
 
 var bodyParser = require('body-parser')
 
-router.post("/login",function(req,res){
+var ocioso = 0, detenido = 0, corriendo = 0, zombie = 0, suspendido = 0;
+
+var info_pocess, estados_cant = new Array();
+
+var userid = require('userid');
+
+const fs = require('fs');
+
+const testFolder = '/proc/';
+
+router.post("/login", function (req, res) {
 
     // res.send("Hola Mundo.");
     console.log(req.body.email);
 
-    if(req.body.email == "admin@admin.com" && req.body.password == 123){
-        res.render("dashboard");
-    }else{
+    if (req.body.email == "admin@admin.com" && req.body.password == 123) {
+        getProcessData();
+        res.render("dashboard", { data1: info_pocess });
+    } else {
         res.render("index");
     }
+});
 
-     
- });
 
- router.use(bodyParser.json())
+function getProcessData() {
+    fs.readdir(testFolder, (err, files) => {
+        files.forEach(file => {
+            if (!isNaN(file)) {
+                var pid = file;
+                file = testFolder + file;
+                fs.readFile(file + "/status", function (err, data) {
+                    info_pocess = new Array();
+                    /**
+                     * Información extraída del archivo /status
+                     * 2: Contiene el estado del proceso
+                     * 8: Contiene el id del usuario que inicio el proceso
+                     * 0: Contiene el nombre del proceso.
+                     * */
+                    var elems = data.toString().split('\n');
+                    var estado = elems[2];
+                    var user = elems[8];
+                    var state = estado.split('	')[1].split(' ');
+                    var id_user = user.split('	');
+                    var name = elems[0].split('	');
+                    console.log("uid name is:", userid.username(parseInt(id_user[1])));
+                    console.log(id_user[1]);
+                    console.log("name process: " + name[1]);
+                    console.log("PID: " + pid);
 
- module.exports = router;
+                    /**
+                     * Cantidad de procesos según su estado:
+                     * S: Sleeping, I: Idlee, T: Stoped, R: Running, Z: Zombie
+                     * Se almacenan los contadores en un array.
+                     */
+
+                    if (state[0] == "S") {
+                        suspendido += 1;
+                    } else if (state[0] == "I") {
+                        ocioso += 1;
+                    } else if (state[0] == "T") {
+                        detenido += 1;
+                    } else if (state[0] == "R") {
+                        corriendo += 1;
+                    } else if (state[0] == "Z") {
+                        zombie += 1;
+                    }
+
+                    estados_cant.push(suspendido);
+                    estados_cant.push(ocioso);
+                    estados_cant.push(detenido);
+                    estados_cant.push(corriendo);
+                    estados_cant.push(zombie);
+
+
+                });
+
+                /**
+                 * Lectura del porcentaje de memoria utilizada por un proceso en el archivo /statm
+                 */
+
+                fs.readFile(file + "/statm", function (err, data) {
+                    var elems = data.toString().split(' ');
+                    var memoria = elems[1];
+                    console.log(file + " Cantidad memoria: " + memoria + "Porcentaje: " + memoria / 10000 + "%");
+                });
+
+                /**
+                  * Almacenamiento de información en arraylist:
+                  * id, user, state, %RAM, name
+                  */
+                info_pocess.push(pid);
+                info_pocess.push(userid.username(parseInt(id_user[1])));
+                info_pocess.push(state[0]);
+                info_pocess.push(memoria / 10000 + "%");
+                info_pocess.push(name[1]);
+
+            }
+        });
+
+    });
+}
+
+
+router.use(bodyParser.json())
+
+module.exports = router;
