6026c7f4c8a5a08820223ce0cd05118c4f5576e8
DanielGarcia097 <2009402600101@ingenieria.usac.edu.gt>
2019-03-10 12:24:20 -0600

    asdf
    asdf

--- practica2/app/routes.js
+++ practica2/app/routes.js
@@ -130,6 +130,20 @@ os2.cpuUsage(function(v){
     console.log( 'CPU Usage (%): ' + v );
 });
 
+var cpus = os.cpus();
+
+for(var i = 0, len = cpus.length; i < len; i++) {
+    console.log("CPU %s:", i);
+    var cpu = cpus[i], total = 0;
+
+    for(var type in cpu.times) {
+        total += cpu.times[type];
+    }
+
+    for(type in cpu.times) {
+        console.log("\t", type, Math.round(100 * cpu.times[type] / total));
+    }
+}
 
 /**
  * PETICIÓN GET PARA LA VISTA DE INFORMACIÓN CPU
